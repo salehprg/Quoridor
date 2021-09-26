771b6c7676ad5ab6d0a7450209c5093b95d8689f
Behzad Shomali <behzad.shomali@gmail.com>
2021-09-26 15:43:51 +0330

    Run a simple test by choosing random actions

--- python/main.py
+++ python/main.py
@@ -1,9 +1,22 @@
+from Player import Player
 from utils import Board
+from time import sleep
+from random import choice
 
 if __name__ == "__main__":
     board = Board()
 
-    for i in range(9):
-        for j in range(9):
-            print(board.get_piece(j, i).d_side, end=" ")
-        print()
+    white_player = Player("white", 4, 8)
+    black_player = Player("black", 4, 0)
+
+    while True:
+        action = choice(white_player.get_legal_moves(black_player, board))
+        white_player.take_action(action, board)
+        board.print_map()
+        print("white: " + action)
+        input()
+        action = choice(black_player.get_legal_moves(white_player, board))
+        black_player.take_action(action, board)
+        board.print_map()
+        print("black: " + action)
+        input()
