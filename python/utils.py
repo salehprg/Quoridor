c1f94d5d713ae10de6b6bc2cb47732bfb6890e5c
Behzad Shomali <behzad.shomali@gmail.com>
2021-09-26 15:42:53 +0330

    Method "print_map" has been added Fixed some minor bugs

--- python/utils.py
+++ python/utils.py
@@ -1,9 +1,20 @@
 from Player import Player
+import os
 
 
 class Piece:
     def __init__(
-        self, x, y, state, r_side, u_side, l_side, d_side, is_white_goal, is_black_goal
+        self,
+        x,
+        y,
+        state,
+        r_side,
+        u_side,
+        l_side,
+        d_side,
+        is_white_goal,
+        is_black_goal,
+        is_border_piece,
     ):
         self.x = x
         self.y = y
@@ -14,6 +25,7 @@ class Piece:
         self.d_side = d_side
         self.is_white_goal = is_white_goal
         self.is_black_goal = is_black_goal
+        self.is_border_piece = is_border_piece
 
     def get_position(self):
         return self.x, self.y
@@ -23,7 +35,7 @@ class Board:
     ROWS_NUM = 9
     COLS_NUM = 9
     map = []
-    origin_pieces_walls = []
+    paired_block_pieces = []
 
     def __init__(self):
         for y in range(self.ROWS_NUM):
@@ -36,12 +48,14 @@ class Board:
                 down_side = "free"
                 is_white_goal = False
                 is_black_goal = False
+                is_border_piece = False
                 if y == 0:
                     is_white_goal = True
                     up_side = "block"
                     if x == 4:
                         state = "black"
                 elif y == self.ROWS_NUM - 1:
+                    is_border_piece = True
                     is_black_goal = True
                     down_side = "block"
                     if x == 4:
@@ -49,6 +63,7 @@ class Board:
                 if x == 0:
                     left_side = "block"
                 elif x == self.COLS_NUM - 1:
+                    is_border_piece = True
                     right_side = "block"
 
                 row.append(
@@ -62,11 +77,14 @@ class Board:
                         down_side,
                         is_white_goal,
                         is_black_goal,
+                        is_border_piece,
                     )
                 )
             self.map.append(row)
 
     def get_piece(self, x, y):
+        x = min(x, self.COLS_NUM - 1)
+        y = min(y, self.ROWS_NUM - 1)
         return self.map[y][x]
 
     def get_white_goal_pieces(self):
@@ -90,22 +108,26 @@ class Board:
 
         return neighbors
 
-    def is_reachable(self, white_player_piece: Player, black_player_piece: Player):
-        for player in [white_player_piece, black_player_piece]:
+    def is_reachable(self, white_player: Player, black_player: Player):
+        for player in [white_player, black_player]:
             destination = (
                 self.get_white_goal_pieces()
-                if player.color == "black"
+                if player.color == "white"
                 else self.get_black_goal_pieces()
             )
-            visited = [False] * (self.ROWS_NUM * self.COLS_NUM)
-            queue = []
+            visited = {}
+            for row in self.map:
+                for piece in row:
+                    visited[piece] = False
 
-            queue.append(player)
-            visited[player] = True
+            player_piece = self.get_piece(*player.get_position())
+
+            queue = []
+            queue.append(player_piece)
+            visited[player_piece] = True
             can_be_reached = False
             while queue:
                 piece = queue.pop(0)
-
                 if piece in destination:
                     can_be_reached = True
                     break
@@ -119,4 +141,63 @@ class Board:
                 return False
 
         return True
+
+    def print_map(self):
+        VERTICAL_WALL = "\u2503"
+        HORIZONTAL_WALL = "\u2501"
+        WHITE_PLAYER = "\u265F"
+        BLACK_PLAYER = "\u2659"
+        SQUARE = "\u00B7"
+
+        os.system("cls" if os.name == "nt" else "clear")
+
+        for y in range(self.ROWS_NUM):
+            for x in range(self.COLS_NUM):
+                if x == 0:
+                    print(VERTICAL_WALL, end=" ")
+
+                piece = self.get_piece(x, y)
+
+                if piece.state == "empty":
+                    print(SQUARE, end=" ")
+                elif piece.state == "white":
+                    print(WHITE_PLAYER, end=" ")
+                else:
+                    print(BLACK_PLAYER, end=" ")
+
+                if piece.r_side == "block":
+                    print(VERTICAL_WALL, end=" ")
+                else:
+                    print(" ", end=" ")
+
+            print()
+
+            if y != self.ROWS_NUM - 1:
+                print(VERTICAL_WALL, end=" ")
+                for x in range(self.COLS_NUM):
+                    piece = self.get_piece(x, y)
+                    if piece.d_side == "block":
+                        end_char = (
+                            HORIZONTAL_WALL
+                            if self.get_piece(x + 1, y).d_side == "block"
+                            else " "
+                        )
+                        print(HORIZONTAL_WALL, end=end_char)
+                    # elif self.get_piece(x - 1, y).d_side == "block":
+                    #     print(HORIZONTAL_WALL, end=" ")
+                    else:
+                        print(" ", end=" ")
+                    if (
+                        piece.r_side == "block"
+                        and self.get_piece(x, y + 1).r_side == "block"
+                    ):
+                        print(VERTICAL_WALL, end=" ")
+                    elif (
+                        piece.d_side == "block"
+                        and self.get_piece(x + 1, y).d_side == "block"
+                    ):
+                        print(HORIZONTAL_WALL, end=HORIZONTAL_WALL)
+                    else:
+                        print(" ", end=" ")
+                print()
 
